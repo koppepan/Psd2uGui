fb682ebdff256ef1a83d2f301affea93c996819f
koppepan <koppepan0114@gmail.com>
2018-03-05 22:37:53 +0900

    Convert時のパラメータを設定するScriptableObject

---
+++ Unity/Assets/Psd2uGui/Editor/Scripts/ConvertParameter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Psd2uGui.Editor
+{
+    class ConvertParameter : ScriptableObject
+    {
+        public Font defaultFont = null;
+
+        public string buttonKey = ".*button.*";
+        public string buttonPressedKey = ".*pressed";
+        public string buttonHighlightedKye = ".*highlighted";
+        public string buttonDisabledKey = ".*disabled";
+
+        public string labelKey = "label_.*";
+    }
+}
